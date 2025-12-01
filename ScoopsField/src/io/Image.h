d4ecdcc3117c3735f6224e42292d84bfcbe384f9
ScewpsDev <mail.faris.m@gmail.com>
2025-12-01 19:04:02 +0100

    moved scoopsfield to its own repo

--- ScoopsField/src/io/Image.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <SDL3/SDL.h>
-
-
-void* LoadASTCImage(const char* path, int* width, int* height, int* size);
-void* LoadDDSImage(const char* path, int* width, int* height, int* size);
--- ScoopsField/src/utils/Simplex.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-
-float Simplex2f(float x, float y);
-float Simplex3f(float x, float y, float z);
-float Simplex4f(float x, float y, float z, float w);
